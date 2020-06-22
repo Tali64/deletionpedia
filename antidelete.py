0a3fa9bd1d7bb1ee2fb79c678490b909fe3986ff
Tali64 <63979637+Tali64@users.noreply.github.com>
2020-06-21 19:18:01 -0500

    Update antidelete.py

--- antidelete.py
+++ antidelete.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# (C) Kasper Souren 2013-2015
 #
-# http://deletionpedia.miraheze.org
+# http://wikiarchive.miraheze.org
 #
 # Script to rescue articles from Wikipedia
 # Multilingual, additions for other languages welcome
